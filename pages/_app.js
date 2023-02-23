4a1464f9409cf4a340ba4a215c2170f0848ae54e
pooja <poojabelaramani51@gmail.com>
2023-02-23 18:10:42 +0530

    added title

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,21 @@
 import '../styles/globals.css'
 import { Layout } from '../components/Layout'
 import { AuthContextProvider } from "../firebase/AuthContextProvider"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthContextProvider>
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-    </AuthContextProvider>
+    <>
+      <Head>
+        <title>Content Crafter</title>
+      </Head>
+
+      <AuthContextProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      </AuthContextProvider>
+    </>
   )
 }
 
